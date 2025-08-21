8a6153d88a0d835d15de5e9c97d908e584cde8b3
Bernardo Flores Dalla Rosa <88604573+Dalla-Rosa@users.noreply.github.com>
2025-08-20 22:11:18 -0300

    Create Circulo.cpp

---
+++ Grafos/Circulo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Circulo.h"
+
+using namespace std;
+
+Circulo::Circulo(double raio) {
+  raio_ = raio;
+}
+
+double Circulo::calcula_area() {
+    return 3.14 * (raio_ * raio_);
+}
+
+void Circulo::imprime_area() {
+    cout << calcula_area() << "\n";  
+};
